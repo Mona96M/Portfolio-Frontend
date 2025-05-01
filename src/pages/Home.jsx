dd73cc59b5a253c879523bd1f4f39acd9f3b6b7c
Mona96M <monaalshahrani96@gmail.com>
2025-05-01 15:07:56 +0300

    Create Home page with styled text and button

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+    <div>
+        <h1>Easily create a professional CV</h1>
+        <p>Finish your resume in minutes...</p>
+        <button type='submit'>Get Start</button>
+    </div>
+    )
+}
+
+export default Home
