91ae0d5961791e7973d794c251b56d0b0ef531d6
Mona96M <monaalshahrani96@gmail.com>
2025-05-02 21:30:31 +0300

    implement API call and display personal info

--- src/components/PortofolioList/PersonalInfoList.jsx
+++ src/components/PortofolioList/PersonalInfoList.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import axios from 'axios'
 
+import { useState, useEffect } from 'react'
+
 function PersonalInfoList() {
+    const [personalInfo, setPersonalInfo] = useState([])
 
     async function getAllInfo(){
-        const response = await axios.get('http://127.0.0.1:8000/personalinfo/')
+        const response = await axios.get('http://127.0.0.1:8000/api/personalinfo/')
         console.log(response)
+        setPersonalInfo(response.data)
     }
-    getAllInfo()
+    useEffect(() => {
+        getAllInfo()
+    }, [])
 
     return (
         <div>
             <h2>All Personal info</h2>
-
+            <ul>
+                {personalInfo.map(personalInf => {
+                    return (
+                        <li>
+                            <p>{personalInf.full_name}</p>
+                            <p>{personalInf.phone}</p>
+                            <p>{personalInf.linkedin}</p>
+                            <p>{personalInf.bio}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
