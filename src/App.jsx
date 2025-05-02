8ca9329543ccac2fa1f16dea6aa33ff979e91c59
Mona96M <monaalshahrani96@gmail.com>
2025-05-02 22:02:11 +0300

    add PortfolioAdd page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
 import Login from './pages/Login'
+import PortofolioAdd from './pages/PortofolioAdd'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>}/>
+            <Route path='/portfolio/add' element={<PortofolioAdd/>} />
         </Routes>
     </Router>
     )
---
+++ src/pages/PortofolioAdd.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PersonalInfoForm from '../components/PortfolioForm/PersonalInfoForm'
+
+function PortofolioAdd() {
+    return (
+    <div>
+        <h1>Fill out the form</h1>
+        <PersonalInfoForm/>
+    </div>
+    )
+}
+
+export default PortofolioAdd
