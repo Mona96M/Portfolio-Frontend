8115023525225d75e086189c2b7c277f83d7bd7b
Mona96M <monaalshahrani96@gmail.com>
2025-05-03 05:43:23 +0300

    implement API call and display education

--- src/components/PortofolioList/EducationList.jsx
+++ src/components/PortofolioList/EducationList.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import axios from 'axios'
+import { useState , useEffect} from 'react'
 
 function EducationList() {
+    const [education, setEducation] = useState([])
+
     async function getAllEducation(){
         const response = await axios.get('http://127.0.0.1:8000/api/educations')
         console.log(response)
+        setEducation(response.data)
     }
+    useEffect(() => {
     getAllEducation()
+}, [])
     return (
     <div>
         <h2>Education list:</h2>
+        <ul>
+                {education.map(educations => {
+                    return (
+                        <li key={educations.id}>
+                            <p>{educations.university}</p>
+                            <p>{educations.major}</p>
+                        </li>
+                    )
+                })}
+            </ul>
     </div>
     )
 }
