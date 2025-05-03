1351de5a27164cd7d0ae4af7fe6147625219fe0f
Mona96M <monaalshahrani96@gmail.com>
2025-05-03 12:47:55 +0300

    Add SkillDetails component and display skill by id

---
+++ src/components/AllDetails/SkillDetails.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { useState , useEffect} from 'react'
+import { useParams } from 'react-router'
+
+import axios from 'axios'
+
+function SkillDetails() {
+    const { id } = useParams()
+    const [skill, setSkill] = useState(null)
+    const [errorMsg, setErrorMsg] = useState('')
+
+    async function getSingleData() {
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/api/skills/${id}`)
+            setSkill(response.data)
+        } catch (err) {
+            console.log(err)
+            if (err.status === 404) {
+                setErrorMsg('Skill Not Found')
+            } else {
+                setErrorMsg('Somethig went Wrong!')
+            }
+        }
+    }
+    useEffect(() => {
+        getSingleData()
+        console.log(id)
+    }, [])
+
+    if (errorMsg) return <h1>{errorMsg}</h1>
+    if (!skill) return <h4>Loading your Skill details...</h4>
+
+    return (
+        <div>
+            <h4>{skill.name_of_skill}</h4>
+        </div>
+    )
+}
+export default SkillDetails
+
--- src/pages/PortfolioDetails.jsx
+++ src/pages/PortfolioDetails.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PersonalInfoDetails from '../components/AllDetails/PersonalInfoDetails'
 import EducationDetails from '../components/AllDetails/EducationDetails'
+import SkillDetails from '../components/AllDetails/SkillDetails'
 
 function PortfolioDetails() {
     return (
     <div>
         <PersonalInfoDetails/>
         <EducationDetails/>
+        <SkillDetails/>
     </div>
     )
 }
