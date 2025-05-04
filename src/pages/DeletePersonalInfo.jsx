84e3bb4c76c24ea7cbb7c21aff9881563b5a0b71
Mona96M <monaalshahrani96@gmail.com>
2025-05-04 09:45:50 +0300

    Add Confirm Delete page to redirect to deletion confirmation page

--- src/pages/DeletePersonalInfo.jsx
+++ src/pages/DeletePersonalInfo.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useParams, useNavigate } from 'react-router'
+import axios from 'axios'
 
 function DeletePersonalInfo() {
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    async function deletePersonalInfo() {
+        try {
+            const response = await axios.delete(`http://127.0.0.1:8000/api/personalinfo/${id}/`)
+            if (response.status === 204) {
+                navigate('/') 
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
-    <div>
-        <h1>Confirm Delete</h1>
-    </div>
+        <div>
+            <h2>Are you sure you want to delete this information?</h2>
+            <button onClick={deletePersonalInfo}>Delete</button>
+            <button onClick={() => navigate(-1)}>Cancel</button>
+        </div>
     )
 }
+
 
 export default DeletePersonalInfo
